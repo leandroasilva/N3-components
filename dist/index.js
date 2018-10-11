d36bdee1aa46dac0f926edecaeb6715ced1eb4b2
leandroasilva <halk_lenson@hotmail.com>
2018-10-11 16:16:23 -0300

    ajust url font

--- dist/index.js
+++ dist/index.js
@@ -26177,7 +26177,7 @@ exports.default = {
         if (_type2.default.isArray(this.currentValue) && this.range) {
           this.currentValue = [Math.min(this.tempValue[0], this.tempValue[1]), Math.max(this.tempValue[0], this.tempValue[1])];
         } else {
-          this.currentValue = this.tempValue[1];
+          this.currentValue = val;
         }
       }
     }
